7cedf44e7faf35f7944f93c5d48111284bb84107
cloxx <duriez.clotilde@gmail.com>
2021-04-08 00:30:39 +0200

    soutenance

--- app/src/main/java/com/example/rdvmanager/Utils.java
+++ app/src/main/java/com/example/rdvmanager/Utils.java
@@ -8,16 +8,14 @@ public class Utils
     private static int sTheme;
     public final static int LightTheme = 0;
     public final static int DarkTheme = 1;
-    /**
-     * Set the theme of the Activity, and restart it by creating a new Activity of the same type.
-     */
+
     public static void changeToTheme(Activity activity, int theme)
     {
         sTheme = theme;
         activity.finish();
         activity.startActivity(new Intent(activity, activity.getClass()));
     }
-    /** Set the theme of the activity, according to the configuration. */
+
     public static void onActivityCreateSetTheme(Activity activity)
     {
         switch (sTheme)
